3bc4749098f6713c446225ae186cfab06c7ff508
ajithmadhan113 <ajithmadhan.dev113@gmail.com>
2021-01-12 13:04:12 +0530

    pushed to server

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+import Footer from './Footer';
+import Navbar from './Navbar';
+import Container from './Container';
+
+function App() {
+  return (
+    <div className="App">
+<Navbar/>
+
+  <Header/>
+<Container/>
+<Footer/>
+
+    </div>
+  );
+}
+
+export default App;
